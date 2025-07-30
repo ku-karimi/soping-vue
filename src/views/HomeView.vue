182b000ab277c5c749811da39a932738d0178d7b
kosar karimi <karimi.z8346@gmail.com>
2025-07-30 11:26:16 +0330

    create header home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,11 @@
 
 <template>
   <div class="header">
+    <div class="headerText">
+      <h2 class="text titr">Hello world</h2>
+      <p class="text p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquam deserunt, voluptatibus culpa molestias voluptas possimus quia delectus sapiente natus.</p>
+    </div>
+  </div>
+  <div class="homeIcons">
   </div>
 </template>
